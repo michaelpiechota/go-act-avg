4fcd47325d66363db79583df7546e29b1d6bd834
Michael Piechota <mepiechota@gmail.com>
2019-12-15 11:08:43 -0800

    Adds data model for AddAction input

--- pkg/models/model.go
+++ pkg/models/model.go
@@ -1,1 +1,9 @@
 package models
+
+// Data model for AddAction input
+// serialized JSON string -> JSON struct conversion
+type Input struct {
+	Action string	`json:"action"`
+	Time int		`json:"time"`
+}
+
